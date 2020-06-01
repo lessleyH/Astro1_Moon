42a04b69d5fccc4a637bd30baf8218781d392ffd
Aadil <aadilislam101@gmail.com>
2020-06-01 19:30:47 -0400

    cleaned up

--- earth_moon.py
+++ earth_moon.py
@@ -1,7 +1,8 @@
-# lots o' imports
 import os, sys, pygame
 import os.path
 import math
+
+# get location of file
 file_path = os.path.dirname(__file__)
 
 # constants
@@ -14,6 +15,14 @@ lunar_distance = 384400 # km
 zoom_size = 75 # window for seeing phases
 WHITE = (255, 255, 255) # colors
 BLACK = (0, 0, 0)
+phase_dict = {  0:'(5) Full',
+                1:'(6) Waning Gibbous',
+                2:'(7) Third Quarter',
+                3:'(8) Waning Crescent',
+                4:'(1) New',
+                5:'(2) Waxing Crescent',
+                6:'(3) First Quarter',
+                7:'(4) Waxing Gibbous'  }
 
 # initialize game
 pygame.init()
@@ -21,42 +30,50 @@ screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Lessley & Aadil\'s Creative Project')
 font = pygame.font.SysFont(None, 24)
 
-# image surfaces
-background = pygame.image.load(os.path.join(file_path, "images/space.jpg"))
+### LOAD SURFACES ###
+
+# background surface
+background = pygame.image.load(os.path.join(file_path, "assets/space.jpg"))
 background = pygame.transform.scale(background, size)
 background = background.convert()
 background_rect = background.get_rect()
 
-earth = pygame.image.load(os.path.join(file_path, "images/earth_from_above_shadow.png"))
+# earth surface
+earth = pygame.image.load(os.path.join(file_path, "assets/earth_from_above_shadow.png"))
 earth = pygame.transform.scale(earth, (int(earth_diameter*km_to_pixel), int(earth_diameter*km_to_pixel)))
 earth = earth.convert()
 earth_rect = earth.get_rect()
 earth_rect.centerx, earth_rect.centery = width/2, height/2
 
-moon = pygame.image.load(os.path.join(file_path, "images/moon_from_above.png"))
+# tiny moon surface
+moon = pygame.image.load(os.path.join(file_path, "assets/moon_from_above.png"))
 moon = pygame.transform.scale(moon, (int(moon_diameter*km_to_pixel), int(moon_diameter*km_to_pixel)))
 moon = moon.convert()
 moon_rect = moon.get_rect()
 moon_rect.centerx, moon_rect.centery = earth_rect.centerx, earth_rect.centery-int(lunar_distance*km_to_pixel)
 
-arrow = pygame.image.load(os.path.join(file_path, "images/arrow.png"))
+# arrow (pointing to tiny moon) surface
+arrow = pygame.image.load(os.path.join(file_path, "assets/arrow.png"))
 arrow = pygame.transform.scale(arrow, (30, 10))
 arrow_rect = arrow.get_rect()
 arrow_rect.centerx, arrow_rect.centery = arrow_rect.centerx, arrow_rect.centery-arrow.get_height()/2
 
-zoom_border = pygame.Rect(arrow_rect.centerx + arrow_rect.width, arrow_rect.centery - zoom_size/2, zoom_size, zoom_size)
+# zoom window suface
 zoom = pygame.Rect(arrow_rect.centerx + arrow_rect.width + 2, arrow_rect.centery - zoom_size/2 + 2, zoom_size-4, zoom_size-4)
+zoom_border = pygame.Rect(arrow_rect.centerx + arrow_rect.width, arrow_rect.centery - zoom_size/2, zoom_size, zoom_size)
 
-moon_zoomed = pygame.image.load(os.path.join(file_path, "images/moon_from_above_shadow.png"))
+# zoomed moon surface
+moon_zoomed = pygame.image.load(os.path.join(file_path, "assets/moon_from_above_shadow.png"))
 moon_zoomed = pygame.transform.scale(moon_zoomed, (int(zoom_size*0.8), int(zoom_size*0.8)))
 moon_zoomed.convert()
 moon_zoomed_rect = moon_zoomed.get_rect()
 moon_zoomed_rect.center = zoom.center
 
+# phase surfaces
 phase_border = pygame.Rect(earth_rect.centerx + 5*earth_rect.width, earth_rect.centery - zoom_size/2, zoom_size, zoom_size)
 phases = []
 for i in range(1, 8+1):
-    phase = pygame.image.load(os.path.join(file_path, "images/phases", str(i)+".png"))
+    phase = pygame.image.load(os.path.join(file_path, "assets", "phase_{}.png".format(i)))
     phase = pygame.transform.scale(phase, (int(zoom_size*0.8),int(zoom_size*0.8)))
     phase.convert()
     phase_rect = phase.get_rect()
@@ -64,27 +81,27 @@ for i in range(1, 8+1):
     phases.append((phase, phase_rect))
 phase_i = -1
 
-zig_zag = pygame.image.load(os.path.join(file_path, "images/zig-zag.png"))
+# zig-zag (separating sun from earth-moon system) surface
+zig_zag = pygame.image.load(os.path.join(file_path, "assets/zig-zag.png"))
 zig_zag = pygame.transform.scale(zig_zag, (50, height))
 zig_zag.convert()
 zig_zag_rect = zig_zag.get_rect()
 zig_zag_rect.center = (150, height/2)
 
-sun = pygame.image.load(os.path.join(file_path, "images/sun.png"))
+# sun surface
+sun = pygame.image.load(os.path.join(file_path, "assets/sun.png"))
 sun = pygame.transform.scale(sun, (int(sun_diameter*km_to_pixel), int(sun_diameter*km_to_pixel)))
 sun.convert()
 sun_rect = sun.get_rect()
 sun_rect.center = (-450, height/2)
 
 # text surfaces
-phase_dict = {0:'(5) Full',1:'(6) Waning Gibbous',2:'(7) Third Quarter',3:'(8) Waning Crescent',
-                4:'(1) New',5:'(2) Waxing Crescent',6:'(3) First Quarter',7:'(4) Waxing Gibbous'}
 title = font.render('Lunar Phases:', True, WHITE)
 phase_title = font.render('Phase seen on Earth:', True, WHITE)
 zoom_title = font.render('Lunar surface:', True, WHITE)
 sun_title = font.render('The Sun:', True, WHITE)
 story = []
-with open(os.path.join(file_path, 'stories/lunar_phases.txt')) as file:
+with open(os.path.join(file_path, 'assets/lunar_phases.txt')) as file:
     story = [font.render(line.rstrip('\n'), True, WHITE) for line in file]
 story_i = 0
 progress = font.render('{} of {}'.format(story_i+1, len(story)), True, WHITE)
@@ -96,19 +113,21 @@ stats.append('Moon\'s diameter = '+f'{moon_diameter:,}'+' km')
 stats.append('Earth <-> Moon distance = '+f'{lunar_distance:,}'+' km')
 stats = [font.render(stat, True, WHITE) for stat in stats]
 
-# animation variables
+### BEGIN ANIMATION ###
+
+# variables
 angle = 0
 speed = 20
 next_tick = 500
 radius = int(lunar_distance*km_to_pixel)
 
-# animation helper
+# method that moves moon along circular path
 def move_coords(angle, radius):
     theta = math.radians(angle)
     return (earth_rect.centerx + radius * math.cos(theta), 
             earth_rect.centery + radius * math.sin(theta))
 
-# animation loop
+# animation
 while True:
     # events
     for event in pygame.event.get():
@@ -116,15 +135,17 @@ while True:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
+                # move forward in story
                 if story_i > 0:
                     story_i -= 1
                     progress = font.render('{} of {}'.format(story_i+1, len(story)), True, WHITE)
             elif event.key == pygame.K_RIGHT:
+                # move backward in story
                 if story_i < len(story)-1:
                     story_i += 1
                     progress = font.render('{} of {}'.format(story_i+1, len(story)), True, WHITE)
 
-    # move moon in orbit
+    # move moon along orbit
     ticks = pygame.time.get_ticks() 
     if ticks > next_tick:
         next_tick += speed
@@ -137,10 +158,10 @@ while True:
         zoom.center = zoom_border.center
         moon_zoomed_rect.center = zoom.center
 
-    # label phase
+    # determine current phase of moon
     phase_label = font.render(phase_dict[phase_i], True, WHITE)
 
-    # display
+    # display everything
     screen.blit(background, (0, 0))
     screen.blit(title, (width//2 - title.get_rect().width//2, 0+title.get_rect().height))
     screen.blit(earth, earth_rect)
@@ -164,4 +185,6 @@ while True:
     for stat_i, stat in enumerate(stats):
         screen.blit(stat, ((width-stat.get_rect().width-10, (stat_i+1)*stat.get_rect().height)))
     screen.blit(progress, ((width-progress.get_rect().width-10, height-2*progress.get_rect().height)))
+
+    # update display
     pygame.display.update()
